a2bdee8e08ac6af5fed1fba828fb5e185a2a41d4
Anton4703 <38693163+Anton4703@users.noreply.github.com>
2018-06-25 17:18:50 +0300

    Add files via upload

---
+++ JS/anton.js
@@ -0,0 +1,15 @@
+$(".tab1").click(function (){
+    $("[class^=hide]").hide();
+    $("[class^=hide1]").show();
+
+});
+
+$(".tab2").click(function (){
+    $("[class^=hide]").hide();
+    $("[class^=hide2]").show();
+});
+
+$(".tab3").click(function (){
+    $("[class^=hide]").hide();
+    $("[class^=hide3]").show();
+});
